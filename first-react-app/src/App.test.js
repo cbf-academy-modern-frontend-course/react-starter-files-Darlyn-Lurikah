97ab47ec8345a1cf7889f452c7318b9c012dfebe
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2022-10-28 18:02:23 +0100

    Test 1 passed

--- first-react-app/src/App.test.js
+++ first-react-app/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReactDOM from "react-dom"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+it('renders without crashing', () => {
+  const root = ReactDOM.createRoot(document.createElement('div'));
+  root.render(<App />,root);
 });
