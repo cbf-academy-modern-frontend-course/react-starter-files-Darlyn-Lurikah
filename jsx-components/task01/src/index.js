82c4f659c1d7861a7aa1d50d2b8dd8e7e5912e53
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2022-11-02 17:39:48 +0000

    Add conditional to display name if variable is declared

--- jsx-components/task01/src/index.js
+++ jsx-components/task01/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const name = '[YOUR NAME GOES HERE]';
+const name = 'Darlyn';
 
 function Profile(name){
+  if (name) {
     return <main id="content" role="main" className="base">
             <h1>{name}'s React Page</h1>
               <p>Example of react JSX in action</p>
             </main>
+  } else {
+    return <main id="content" role="main" className="base">
+            <h1>Just a React Page</h1>
+            </main>
+  }
+    
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Profile());
+root.render(Profile(name));
 
