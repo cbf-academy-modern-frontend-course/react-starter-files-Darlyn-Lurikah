c5c451cdc3464e4255540550bacd166382074aa9
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2022-11-03 16:37:00 +0000

    Edited profile function

--- jsx-components/task01/src/index.js
+++ jsx-components/task01/src/index.js
@@ -1,25 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Profile from './Profile'
 
-const name = 'Darlyn';
-const lowerName = name.toLowerCase();
 
-function Profile(name){
-  
-  if (lowerName === ('Darlyn'.toLowerCase())) {
-    
-    return <main id="content" role="main" className="base">
-            <h1>{name}'s React Page</h1>
-              <p>Example of react JSX in action</p>
-            </main>
-  } else {
-    return <main id="content" role="main" className="base">
-            <h1>Just a React Page</h1>
-            </main>
-  }
-    
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Profile(name));
-
+root.render(<Profile name="Darlyn" bio="Something about me"/>);
