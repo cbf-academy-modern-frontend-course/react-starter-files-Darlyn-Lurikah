5ff8bfbd041d68f0ddb1d0b33cb1544e9e14a05e
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2022-11-06 19:15:24 +0000

    Debug image issue. JSON  Images now displaying

--- props-and-state/src/components/GadgetList.js
+++ props-and-state/src/components/GadgetList.js
@@ -1,21 +1,29 @@
 import React from 'react';
 
-
-
+const reactElement = (key, src, title, price, headphonesStyle, priceStyle) => {
+  console.log(src)
+  return (
+    <div key={key}>
+      <img src={src} alt={title}/>
+      <div style={headphonesStyle}>{title}</div>
+      <div style={priceStyle}>£{price}</div>
+      <hr></hr>   
+    </div>
+  )
+  
+}
 const GadgetList = (props) => {
 
   const headphonesStyle = {"fontWeight": "bold"}
-  const priceStyle = {"color": "red"};
+  const priceStyle = {
+    "color": "red",
+    "fontWeight": "bold"
+  };
 
   return (
     <main>
       <h2>Headphones</h2>
-      {props.items.map((item) => (<div key={item.key}><img scr={item.src} alt={item.title}></img><div style={headphonesStyle}>{item.title}
-        <div style={priceStyle}>£{item.price}</div><hr></hr></div>
-        <div>
-        
-        </div>
-        </div> ))}
+      {props.items.map((item) => reactElement(item.key, item.src, item.title, item.price, headphonesStyle, priceStyle) )}
     </main>
   );
 }
