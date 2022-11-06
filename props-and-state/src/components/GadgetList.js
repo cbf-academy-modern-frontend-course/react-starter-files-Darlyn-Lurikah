16c354e75aff75f25466c524a8e70cb0b3562f1e
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2022-11-06 15:00:45 +0000

    Task 1 - add to headphone object and add styling

--- props-and-state/src/components/GadgetList.js
+++ props-and-state/src/components/GadgetList.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+
+
 const GadgetList = (props) => {
+
+  const headphonesStyle = {"fontWeight": "bold"}
+  const priceStyle = {"color": "red"};
 
   return (
     <main>
       <h2>Headphones</h2>
-      {props.items.map((item) => (<div>{item.title}</div>))}
+      {props.items.map((item) => (<div><div style={headphonesStyle}>{item.title}
+        <div style={priceStyle}>£{item.price}</div><hr></hr></div>
+        </div> ))}
     </main>
   );
 }
--- props-and-state/src/index.js
+++ props-and-state/src/index.js
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom/client';
 
 import GadgetList from './components/GadgetList';
 const mainStyle = {"fontFamily":"Arial"};
+const storeTitle = {"fontSize": "xx-large"}
 
 const App = () => {
-  const headphones =  [{"title":"Headphones 1"},{"title":"Headphones 2"},{"title":"Headphones 3"},{"title":"Headphones 4"},];
+  const headphones =  [{"title":"Headphones 1", "price": 255.99},{"title":"Headphones 2", "price": 305.00},{"title":"Headphones 3", "price": 170.00},{"title":"Headphones 4", "price": 205.99},{"title":"Headphones 5", "price": 384.99}];
   const [headphonesList] = useState(headphones);
 
   return <section style={mainStyle}>
-    <h1>Electronics Store</h1>
+    <h1 style={storeTitle}>Electronics Store</h1>
     <GadgetList items={headphonesList}/>
   </section>;
 }
