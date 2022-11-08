5c0d0526e5be616d48495ff81124694f140bbc33
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2022-11-08 19:42:50 +0000

    Book compomonent title, author and image displaying

--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Book({book}) {
-  return <h1>{book.volumeInfo.title}</h1>;
-}
+  return <div>
+      <h1>{book.volumeInfo.title}</h1>
+      <p>{book.volumeInfo.authors}</p>
+      <img src={book.volumeInfo.imageLinks.thumbnail}></img>
+    </div>
+};
 
 export default Book;
