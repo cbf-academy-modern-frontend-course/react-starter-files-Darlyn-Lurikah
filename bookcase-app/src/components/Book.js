81c65c5ca2ba166a51a500576c995fbeb2971022
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2022-11-08 19:55:48 +0000

    Book component price displaying from json file

--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 function Book({book}) {
   return <div>
       <h1>{book.volumeInfo.title}</h1>
       <p>{book.volumeInfo.authors}</p>
-      <img src={book.volumeInfo.imageLinks.thumbnail}></img>
+      <img src={book.volumeInfo.imageLinks.thumbnail} alt=""/>
+      {book ? (
+      <p>£{book.saleInfo.retailPrice && book.saleInfo.retailPrice.amount}</p>
+      ) : (
+        <p>No price found</p>
+      )}
+    
     </div>
 };
 
