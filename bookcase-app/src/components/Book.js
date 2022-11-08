7588b3724c4948c63a38aea9b928b06467f0af9d
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2022-11-08 20:02:46 +0000

    Add conditional to check number of authors and join them with and

--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -4,7 +4,12 @@ import React, { useState } from 'react';
 function Book({book}) {
   return <div>
       <h1>{book.volumeInfo.title}</h1>
-      <p>{book.volumeInfo.authors}</p>
+      {book.volumeInfo.authors.length > 1 ? (
+        <p>{book.volumeInfo.authors.join(" and ")}</p>
+      ) : (
+        <p>{book.volumeInfo.authors}</p>
+      )}
+      
       <img src={book.volumeInfo.imageLinks.thumbnail} alt=""/>
       {book ? (
       <p>£{book.saleInfo.retailPrice && book.saleInfo.retailPrice.amount}</p>
--- bookcase-app/src/index.js
+++ bookcase-app/src/index.js
@@ -2,12 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
