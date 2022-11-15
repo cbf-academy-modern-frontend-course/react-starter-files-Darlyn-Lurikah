f371630d087317709bddf6f3820b30b7d2a71ed6
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2022-11-15 21:16:13 +0000

    Add AboutUs route and Petform component

--- routes-and-forms/src/index.js
+++ routes-and-forms/src/index.js
@@ -5,21 +5,25 @@ import Home from "./components/Home";
 import Kittens from "./components/Kittens";
 import Puppies from "./components/Puppies";
 import Ducklings from "./components/Ducklings";
+import AboutUs from "./components/AboutUs";
+
 
 export default function App() {
   return (
     <Router>
       <ul className="breadcrumb">
-        <li><Link to="/"> All Pets </Link></li>
+        <li><Link to="/">All Pets</Link></li>
         <li><Link to="/kittens"> Kittens </Link></li>
         <li><Link to="/puppies"> Puppies </Link></li>
         <li><Link to="/ducklings"> Ducklings </Link></li>
+        <li><Link to="/aboutus"> About Us </Link></li>
       </ul>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home/>}/>
         <Route path="kittens" element={<Kittens />} />
         <Route path="puppies" element={<Puppies />} />
         <Route path="ducklings" element={<Ducklings />} />
+        <Route path="aboutus" element={<AboutUs />} />
       </Routes>
   </Router>
   );
