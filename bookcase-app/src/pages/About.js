72ac13e4a5fc4b8674239370318c0737e8ed23e9
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2022-11-22 19:11:17 +0000

    Adding styling to the about page

--- bookcase-app/src/pages/About.js
+++ bookcase-app/src/pages/About.js
@@ -1,20 +1,23 @@
 import Header from "../components/Header"
+import './About.css'
 
 function About () {
     return (
         <div>
             <Header/>
-            <header>
+            <div className="about-bg">
                 <h3>Welcome to the Bookcase application</h3>
-            </header>
-            <p>The following application was created by Darlyn. 
+                <p>
+                The following application was created by Darlyn. 
                 this bookcase app displays a list of books that a user
                 can save to a local bookcase. 
 
                 Click on the 'Add +' button to add a book to your bookcase.
                 Use the search bar to find the latest books by name, author, 
                 or description.
-            </p>
+                </p>
+            </div>
+            
         </div>
     )
 }
