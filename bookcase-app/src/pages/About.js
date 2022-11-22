f677e689a935bbec05f2cebe7ac8a23d43890588
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2022-11-22 18:56:21 +0000

    Added to about text

--- bookcase-app/src/pages/About.js
+++ bookcase-app/src/pages/About.js
@@ -4,8 +4,17 @@ function About () {
     return (
         <div>
             <Header/>
-            <header>Welcome to the Bookcase application</header>
-            <p>Something about the application</p>
+            <header>
+                <h3>Welcome to the Bookcase application</h3>
+            </header>
+            <p>The following application was created by Darlyn. 
+                this bookcase app displays a list of books that a user
+                can save to a local bookcase. 
+
+                Click on the 'Add +' button to add a book to your bookcase.
+                Use the search bar to find the latest books by name, author, 
+                or description.
+            </p>
         </div>
     )
 }
