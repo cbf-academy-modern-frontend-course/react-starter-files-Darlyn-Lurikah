cb83161d0b9f8d0ff96d09ad972dfc407cb4b227
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2022-11-23 17:19:50 +0000

    Add onChange to capture input keyword. Displaying

--- bookcase-app/src/components/Search.js
+++ bookcase-app/src/components/Search.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = (props) => {
+
+    const [keyword, setKeyword] = useState('');
+
     return (
         <form>
             <label>Search</label>
-            <input type="text" id="query" name="query"/>
+            <input type="text" value={keyword} onChange={(e) => setKeyword(e.target.value)}/>
             <input type="submit" value="submit"/>
+            <p style={{color:"red"}}><em>{keyword && 'Keywords Typed: ' + keyword}</em></p>
         </form>
     )
 }
